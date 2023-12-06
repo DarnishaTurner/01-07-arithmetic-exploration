e08c1d326cdad4e720fb11eb13ca099f502322a8
Steven Zhang <steven@pursuit.org>
2023-12-06 14:29:48 -0500

    Initial commit with README and .gitignore

---
+++ README.md
@@ -0,0 +1,17 @@
+# Arithmetic Exploration
+
+## Description
+Explore basic arithmetic operations in JavaScript.
+
+## Objective
+- Perform addition, subtraction, multiplication, and division on provided numbers.
+- Console log the results of each operation.
+
+## Setup
+1. Fork and clone the repository.
+2. Navigate to the `01-07-arithmetic-exploration` directory.
+3. Fill in the `index.js` according to the instructions.
+
+## Test Cases
+- Correctly perform each arithmetic operation.
+- Console log should display the results of each operation.
---
+++ index.js
@@ -0,0 +1,21 @@
+// Numbers for arithmetic operations
+const num1 = 8;
+const num2 = 4;
+
+// TODO: Add num1 and num2, and store the result in a variable 'sum'.
+let sum; // Replace this line with your code.
+
+// TODO: Subtract num2 from num1, and store the result in a variable 'difference'.
+let difference; // Replace this line with your code.
+
+// TODO: Multiply num1 and num2, and store the result in a variable 'product'.
+let product; // Replace this line with your code.
+
+// TODO: Divide num1 by num2, and store the result in a variable 'quotient'.
+let quotient; // Replace this line with your code.
+
+// Log Results
+console.log(`Sum: ${sum}`);
+console.log(`Difference: ${difference}`);
+console.log(`Product: ${product}`);
+console.log(`Quotient: ${quotient}`);
